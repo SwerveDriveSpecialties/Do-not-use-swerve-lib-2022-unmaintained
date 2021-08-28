2b3f28881d9d6bff2311937c2390537f874553d5
Jacob Bublitz <jacob@bublitz.com>
2021-08-28 14:52:06 -0700

    Change test function names

--- src/test/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurationsTest.java
+++ src/test/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurationsTest.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SdsModuleConfigurationsTest {
     @Test
-    void mk3StandardOverallReduction() {
+    void mk3StandardReductions() {
         assertEquals(1.0 / 8.16, SdsModuleConfigurations.MK3_STANDARD.getDriveReduction(), 1e-4);
         assertEquals(1.0 / 12.8, SdsModuleConfigurations.MK3_STANDARD.getSteerReduction(), 1e-4);
     }
 
     @Test
-    void mk3FastOverallDriveReduction() {
+    void mk3FastReductions() {
         assertEquals(1.0 / 6.86, SdsModuleConfigurations.MK3_FAST.getDriveReduction(), 1e-4);
         assertEquals(1.0 / 12.8, SdsModuleConfigurations.MK3_FAST.getSteerReduction(), 1e-4);
     }
