86bb1757b672ce5aca8587e7ea09c269aa36a34c
Jacob Bublitz <jacob@bublitz.com>
2021-08-28 13:30:25 -0700

    Change inversion for Mk3 steering

--- src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
+++ src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
@@ -7,7 +7,7 @@ public final class SdsModuleConfigurations {
             (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 60.0),
             true,
             (15.0 / 32.0) * (10.0 / 60.0),
-            false
+            true
     );
     public static final ModuleConfiguration MK3_FAST = new ModuleConfiguration(
             "SDS-Mk3F",
@@ -15,7 +15,7 @@ public final class SdsModuleConfigurations {
             (16.0 / 48.0) * (28.0 / 16.0) * (15.0 / 60.0),
             true,
             (15.0 / 32.0) * (10.0 / 60.0),
-            false
+            true
     );
 
     private SdsModuleConfigurations() {
