103b517068b4dc31e9a6171df3f75995cef54d4c
Jacob Bublitz <jacob@bublitz.com>
2021-08-28 15:19:45 -0700

    Add documentation for module configuration

--- src/main/java/com/swervedrivespecialties/swervelib/ModuleConfiguration.java
+++ src/main/java/com/swervedrivespecialties/swervelib/ModuleConfiguration.java
@@ -2,6 +2,13 @@ package com.swervedrivespecialties.swervelib;
 
 import java.util.Objects;
 
+/**
+ * A swerve module configuration.
+ * <p>
+ * A configuration represents a unique mechanical configuration of a module. For example, the Swerve Drive Specialties
+ * Mk3 swerve module has two configurations, standard and fast, and therefore should have two configurations
+ * ({@link SdsModuleConfigurations#MK3_STANDARD} and {@link SdsModuleConfigurations#MK3_FAST} respectively).
+ */
 public class ModuleConfiguration {
     private final double wheelDiameter;
     private final double driveReduction;
@@ -10,6 +17,19 @@ public class ModuleConfiguration {
     private final double steerReduction;
     private final boolean steerInverted;
 
+    /**
+     * Creates a new module configuration.
+     *
+     * @param wheelDiameter  The diameter of the module's wheel in meters.
+     * @param driveReduction The overall drive reduction of the module. Multiplying motor rotations by this value
+     *                       should result in wheel rotations.
+     * @param driveInverted  Whether the drive motor should be inverted. If there is an odd number of gea reductions
+     *                       this is typically true.
+     * @param steerReduction The overall steer reduction of the module. Multiplying motor rotations by this value
+     *                       should result in rotations of the steering pulley.
+     * @param steerInverted  Whether the steer motor should be inverted. If there is an odd number of gear reductions
+     *                       this is typically true.
+     */
     public ModuleConfiguration(double wheelDiameter, double driveReduction, boolean driveInverted,
                                double steerReduction, boolean steerInverted) {
         this.wheelDiameter = wheelDiameter;
@@ -19,22 +39,41 @@ public class ModuleConfiguration {
         this.steerInverted = steerInverted;
     }
 
+    /**
+     * Gets the diameter of the wheel in meters.
+     */
     public double getWheelDiameter() {
         return wheelDiameter;
     }
 
+    /**
+     * Gets the overall reduction of the drive system.
+     * <p>
+     * If this value is multiplied by drive motor rotations the result would be drive wheel rotations.
+     */
     public double getDriveReduction() {
         return driveReduction;
     }
 
+    /**
+     * Gets if the drive motor should be inverted.
+     */
     public boolean isDriveInverted() {
         return driveInverted;
     }
 
+    /**
+     * Gets the overall reduction of the steer system.
+     * <p>
+     * If this value is multiplied by steering motor rotations the result would be steering pulley rotations.
+     */
     public double getSteerReduction() {
         return steerReduction;
     }
 
+    /**
+     * Gets if the steering motor should be inverted.
+     */
     public boolean isSteerInverted() {
         return steerInverted;
     }
