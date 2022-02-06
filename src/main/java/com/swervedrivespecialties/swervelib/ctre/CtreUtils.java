1bf0660bee79c8cb529de8bf5f8c594bb3b30d85
Colin Wong <cwongmath@gmail.com>
2022-02-05 20:53:32 -0600

    Fix #5 by checking if robot is not in sim

--- src/main/java/com/swervedrivespecialties/swervelib/ctre/CtreUtils.java
+++ src/main/java/com/swervedrivespecialties/swervelib/ctre/CtreUtils.java
@@ -3,12 +3,14 @@ package com.swervedrivespecialties.swervelib.ctre;
 import com.ctre.phoenix.ErrorCode;
 import edu.wpi.first.wpilibj.DriverStation;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 public final class CtreUtils {
     private CtreUtils() {
     }
 
     public static void checkCtreError(ErrorCode errorCode, String message) {
-        if (errorCode != ErrorCode.OK) {
+        if (RobotBase.isReal() && errorCode != ErrorCode.OK) {
             DriverStation.reportError(String.format("%s: %s", message, errorCode.toString()), false);
         }
     }
